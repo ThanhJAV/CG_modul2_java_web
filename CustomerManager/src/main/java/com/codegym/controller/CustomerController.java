c91fcee66f1586855c47391a7b0d9db867dce205
= <=>
2019-10-25 21:52:18 +0700

    Customer manager Model and view

--- CustomerManager/src/main/java/com/codegym/controller/CustomerController.java
+++ CustomerManager/src/main/java/com/codegym/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -48,7 +49,6 @@ public class CustomerController {
             ModelAndView modelAndView = new ModelAndView("/edit");
             modelAndView.addObject("customer", customer);
             return modelAndView;
-
         }else {
             ModelAndView modelAndView = new ModelAndView("/error.404");
             return modelAndView;
@@ -56,11 +56,11 @@ public class CustomerController {
     }
 
     @PostMapping("/edit-customer")
-    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer){
+    public ModelAndView updateCustomer(@ModelAttribute("product") Customer customer , RedirectAttributes redirect){
         customerService.save(customer);
-        ModelAndView modelAndView = new ModelAndView("/edit");
-        modelAndView.addObject("customer", customer);
-        modelAndView.addObject("message", "Customer updated successfully");
+        ModelAndView modelAndView = new ModelAndView("redirect:customers");
+//        modelAndView.addObject("customer", customer);
+        redirect.addFlashAttribute("message", "Customer updated successfully");
         return modelAndView;
     }
 
